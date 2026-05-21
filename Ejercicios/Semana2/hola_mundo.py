bfe6912480aaea5ddb563861b766d0b5266d6e1a
1725111102-kim <1725111102@utectulancingo.edu.mx>
2026-05-21 08:08:57 -0600

    Update hola_mundo.py

--- Ejercicios/Semana2/hola_mundo.py
+++ Ejercicios/Semana2/hola_mundo.py
@@ -1,1 +1,12 @@
+class HolaMundo:
+  def metodoUno(self):
+    print("Metodo uno")
 
+def MetodoDos(self, numero_uno:int, numero_dos:int)-> int:
+  """
+  Este metodo realiza la suma de dos numeros enteros y regresa el resultado
+
+  Args: 
+      numero_uno:int 
+  resultado = numero_uno + numero_dos
+  return resultado
